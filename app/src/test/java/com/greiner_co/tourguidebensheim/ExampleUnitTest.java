07fc8c654fbea466bc464226555b219a9797e907
Jens Greiner <jens.greiner@me.com>
2017-06-12 13:01:17 +0200

    renamed project

--- app/src/test/java/com/greiner_co/tourguidebensheim/ExampleUnitTest.java
+++ app/src/test/java/com/greiner_co/tourguidebensheim/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.greiner_co.tourguidbensheim;
+package com.greiner_co.tourguidebensheim;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
