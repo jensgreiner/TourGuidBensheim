2b828684cc0c51852d96afa1fc9b73aedcc1f6be
Jens Greiner <jens.greiner@me.com>
2017-06-15 19:54:27 +0200

    added fragment_attractions.xml and started to implement SightsFragment.java with inflating the list and adding a header view - to be completed

--- app/src/main/java/com/greiner_co/tourguidebensheim/SightsFragment.java
+++ app/src/main/java/com/greiner_co/tourguidebensheim/SightsFragment.java
@@ -1,17 +1,22 @@
 package com.greiner_co.tourguidebensheim;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SightsFragment extends Fragment {
+
+    // Store the current context
+    Context currentContext;
 
 
     public SightsFragment() {
@@ -22,9 +27,28 @@ public class SightsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+
+        View rootView = inflater.inflate(R.layout.fragment_attractions, container, false);
+
+        // Get the current context and store it for later use
+        currentContext = getActivity().getApplicationContext();
+
+        ImageView imageHeader = new ImageView(currentContext);
+        imageHeader.setImageResource(R.drawable.bensheim_panorama_01_2);
+        String imageContentDescription = getResources().getString(R.string.image_header_sights);
+        imageHeader.setContentDescription(imageContentDescription);
+
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_list.xml file.
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+
+        listView.addHeaderView(imageHeader);
+
+
+        return rootView;
     }
 
 }
